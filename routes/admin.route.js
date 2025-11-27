f40525d41ccf0d3e74d45ed01a0e2aceb190c3fe
riddhilahare14 <lahare.riddhi14@gmail.com>
2025-11-27 12:47:18 +0530

    minor change

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -4,9 +4,6 @@ const router = express.Router();
 const adminController = require('../controllers/admin.controller');
 const { authMiddleware, adminOnly } = require('../middlewares/auth.middleware');
 
-// Temporary route for development only
-router.post('/create-admin', adminController.createAdmin);
-
 router.post('/create-team', authMiddleware, adminOnly, adminController.adminCreateTeam);
 router.post('/create-member', authMiddleware, adminOnly, adminController.adminCreateMember);
 router.post('/create-head', authMiddleware, adminOnly, adminController.adminCreateHead);
